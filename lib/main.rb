0e0e1aa8addb09a245cb8b370420d5125ad58fc3
Reflection <reflectionoflines@gmail.com>
2023-01-31 17:00:07 -0600

    Download and place 10000 word text file in main folder.

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,10 @@
 require_relative 'game'
 require_relative 'player'
 require_relative 'computer'
+require 'open-uri'
+
+download = URI.open('https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt')
+IO.copy_stream(download, '../10000-words.txt')
 
 def greeting
   puts 'Hello and welcome to Hangman user!'

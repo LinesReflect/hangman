c373f7b1534bb7976c1e15d8070102263941a95c
Reflection <reflectionoflines@gmail.com>
2023-01-31 17:18:22 -0600

    Allow the computer to pick a random 5-12 letter word from the 10000 word list.

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,7 +1,18 @@
+require_relative 'word_chooser'
+
 class Computer
+  include WordChooser
   attr_accessor :role
 
   def initialize(role)
     @role = role
+  end
+
+  def choose_word
+    word_list = File.open('../10000-words.txt').each_line.collect do |line|
+      line.downcase
+    end
+    word = word_list.sample
+    (word.length - 1).between?(5, 12) ? word : choose_word
   end
 end

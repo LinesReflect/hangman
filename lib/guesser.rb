7eb6b85eeba7747bcce0961ff69ea88be666490b
Reflection <reflectionoflines@gmail.com>
2023-02-02 12:16:50 -0600

    Check if a guess is acceptable.
    The methods will check if a guess is valid based on its length, if it
    is in the alphabet, and if it has been guessed already.

--- lib/guesser.rb
+++ lib/guesser.rb
@@ -1,11 +1,35 @@
 module Guesser
+  @@guessed_letters = []
+
   def guess_letter
     puts 'Guess a letter!'
     guess = gets.chomp
-    valid_guess?(guess) ? guess : [puts('Not valid!'), guess_letter]
+    if valid_guess?(guess)
+      @@guessed_letters.push(guess)
+      guess
+    else
+      puts 'Not valid!'
+      guess_letter
+    end
+  end
+
+  def valid_length?(guess)
+    guess.length == 1
+  end
+
+  def valid_charachter?(guess)
+    guess.chars.all? { |char| ('a'..'z').include?(char.downcase) }
+  end
+
+  def valid_letter?(guess)
+    @@guessed_letters.include?(guess)
   end
 
   def valid_guess?(guess)
-    guess.length == 1 && guess.chars.all? { |char| ('a'..'z').include?(char.downcase) }
+    valid_length?(guess) && valid_charachter?(guess) && !@@guessed_letters.include?(guess)
+  end
+
+  def guessed_letters
+    @@guessed_letters
   end
 end

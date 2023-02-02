0ef780011af8bb3724fa66bbfa84d422784c29ef
Reflection <reflectionoflines@gmail.com>
2023-02-02 14:30:06 -0600

    Ask player if they want to play again. Start a new game if yes.

--- lib/main.rb
+++ lib/main.rb
@@ -6,9 +6,20 @@ require 'open-uri'
 download = URI.open('https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt')
 IO.copy_stream(download, '../10000-words.txt')
 
-def greeting
-  puts 'Hello and welcome to Hangman user!'
+def start_game
   Game.new
+  play_again
 end
 
-greeting
+def play_again
+  puts "Would you like to play again? Y/N"
+  answer = gets.chomp
+  if ['Y', 'y', 'N', 'n'].include?(answer)
+    answer == 'Y' ? start_game : return
+  else
+    play_again
+  end
+end
+
+puts 'Hello and welcome to Hangman user!'
+start_game
